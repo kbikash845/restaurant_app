3c3ca1d251b204c013f3627e8463913c9588e8d8
Bikash <kbikash845@gmail.com>
2023-06-15 13:43:50 +0530

    Changes

--- src/App.js
+++ src/App.js
@@ -2,11 +2,16 @@
 import { Fragment } from 'react';
 import './App.css';
 import Header from './Component/Layout/Header';
+// import Meals from './Component/Meals/Mealss';
+import Mealss from './Component/Meals/Mealss';
 
 function App() {
   return (
     <Fragment >
      <Header/>
+     <main>
+     <Mealss/>
+     </main>
     </Fragment>
   );
 }
