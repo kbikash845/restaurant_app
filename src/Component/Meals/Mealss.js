4ae39a626d097453e9d390479dcee803aeff1b4b
Bikash <kbikash845@gmail.com>
2023-06-15 13:38:32 +0530

    meal summry and available in mealss component redr

---
+++ src/Component/Meals/Mealss.js
@@ -0,0 +1,16 @@
+//  import { Fragment }from 'react'
+ import MealsSummary from './MealsSummary'
+ import AvailableMeals from './AvailableMeals'
+import { Fragment } from 'react'
+ 
+ function Mealss() {
+   return (
+     <Fragment>
+       <MealsSummary/>
+      <AvailableMeals/>
+     </Fragment>
+   )
+ }
+ 
+ export default Mealss
+ 
