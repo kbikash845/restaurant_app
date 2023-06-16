60cdf8dfc67f0c94451677d658057cfc14d0a97b
Bikash <kbikash845@gmail.com>
2023-06-16 16:27:30 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import './App.css';
 import Header from './Component/Layout/Header';
 // import Meals from './Component/Meals/Mealss';
 import Mealss from './Component/Meals/Mealss';
+import Card from './Component/Cart/Card';
 
 function App() {
   return (
     <Fragment >
+    <Card/>
      <Header/>
      <main>
      <Mealss/>
